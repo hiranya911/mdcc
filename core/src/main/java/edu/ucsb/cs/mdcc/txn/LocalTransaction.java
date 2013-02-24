f0aaab020cf2533ca7037a21b6cc6272f090ee5f
Michael Agun <mikeagun@gmail.com>
2013-02-23 18:22:19 -0800

    simplified fast quorum sizes and added config file support

--- core/src/main/java/edu/ucsb/cs/mdcc/txn/LocalTransaction.java
+++ core/src/main/java/edu/ucsb/cs/mdcc/txn/LocalTransaction.java
@@ -1,9 +1,11 @@
 package edu.ucsb.cs.mdcc.txn;
 
+import edu.ucsb.cs.mdcc.config.MDCCConfiguration;
 import edu.ucsb.cs.mdcc.paxos.AppServer;
 import edu.ucsb.cs.mdcc.paxos.Result;
 
 import java.util.Collection;
+import java.util.Properties;
 
 public class LocalTransaction extends Transaction {
 
@@ -11,11 +13,10 @@ public class LocalTransaction extends Transaction {
 
     public LocalTransaction() {
         super();
-        String[] hosts = { "localhost", "localhost" , "localhost", "localhost", "localhost"};
-        int[] ports = { 7911, 7912, 7913, 7914, 7915 };
-        String procId = "proc0";
-
-        this.appServer = new AppServer(hosts, ports, procId);
+        Properties prop = new Properties();
+        prop.setProperty("mdcc.myid", "local");
+        prop.setProperty("mdcc.server.node1", "localhost:7911");
+        this.appServer = new AppServer(new MDCCConfiguration(prop));
     }
 
     @Override
