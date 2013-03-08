7b5fea78bf1ccf60c8f43385616471da0ac1fa54
Hiranya Jayathilaka <hiranya911@gmail.com>
2013-03-07 19:26:06 -0800

    Using string encoding in fund transfer test

--- core/src/main/java/edu/ucsb/cs/mdcc/txn/FundsTransferTest.java
+++ core/src/main/java/edu/ucsb/cs/mdcc/txn/FundsTransferTest.java
@@ -2,7 +2,6 @@ package edu.ucsb.cs.mdcc.txn;
 
 import edu.ucsb.cs.mdcc.paxos.Transaction;
 import edu.ucsb.cs.mdcc.paxos.TransactionException;
-import org.apache.hadoop.hbase.util.Bytes;
 
 import java.nio.ByteBuffer;
 import java.util.Random;
@@ -25,9 +24,9 @@ public class FundsTransferTest {
         System.out.println("Step 1: Initializing accounts");
         Transaction t1 = fac.create();
         t1.begin();
-        t1.write(ROOT, ByteBuffer.wrap(Bytes.toBytes(startingTotal)));
+        t1.write(ROOT, toBytes(startingTotal));
         for (int i = 0; i < accounts; i++) {
-            t1.write(CHILD + i, ByteBuffer.wrap(Bytes.toBytes(0)));
+            t1.write(CHILD + i, toBytes(0));
         }
         t1.commit();
         System.out.println("Transaction 1 completed...");
@@ -36,20 +35,21 @@ public class FundsTransferTest {
         System.out.println("Step 2: Distributing funds");
         Transaction t2 = fac.create();
         t2.begin();
-        int root = Bytes.toInt(t2.read(ROOT).array());
+        int root = toInt(t2.read(ROOT));
         System.out.println("ROOT = " + root);
         int[] children = new int[accounts];
         for (int i = 0; i < accounts; i++) {
-            int child = Bytes.toInt(t2.read(CHILD + i).array());
+            int child = toInt(t2.read(CHILD + i));
             children[i] = child;
             System.out.println("CHILD" + i + " = " + child);
         }
 
         int fundsPerChild = root/accounts;
         for (int i = 0; i < accounts; i++) {
-            t2.write(CHILD + i, ByteBuffer.wrap(Bytes.toBytes(children[i] + fundsPerChild)));
+            t2.write(CHILD + i, toBytes(children[i] + fundsPerChild));
+            root -= fundsPerChild;
         }
-        t2.write(ROOT, ByteBuffer.wrap(Bytes.toBytes(0)));
+        t2.write(ROOT, toBytes(root));
         t2.commit();
         System.out.println("Transaction 2 completed...");
         System.out.println("==========================================\n");
@@ -75,12 +75,12 @@ public class FundsTransferTest {
         Transaction t4 = fac.create();
         t4.begin();
         int total = 0;
-        root = Bytes.toInt(t4.read(ROOT).array());
+        root = toInt(t4.read(ROOT));
         System.out.println("ROOT = " + root);
         children = new int[accounts];
         total += root;
         for (int i = 0; i < accounts; i++) {
-            int child = Bytes.toInt(t4.read(CHILD + i).array());
+            int child = toInt(t4.read(CHILD + i));
             children[i] = child;
             total += child;
             System.out.println(CHILD + i + " = " + child);
@@ -90,6 +90,16 @@ public class FundsTransferTest {
         System.out.println("Transaction 4 completed...");
         System.out.println("==========================================\n");
         System.out.println("FINAL TOTAL = " + total);
+    }
+
+    private static ByteBuffer toBytes(int value) {
+        String str = String.valueOf(value);
+        return ByteBuffer.wrap(str.getBytes());
+    }
+
+    private static int toInt(ByteBuffer value) {
+        String str = new String(value.array());
+        return Integer.parseInt(str);
     }
 
     private static class FundTransferTask implements Runnable {
@@ -106,17 +116,17 @@ public class FundsTransferTest {
             try {
                 try {
                     Thread.sleep(new Random().nextInt(10) * 500);
-                } catch (InterruptedException e) {
+                } catch (InterruptedException ignored) {
                 }
                 Transaction t3 = fac.create();
                 t3.begin();
-                int root = Bytes.toInt(t3.read(ROOT).array());
+                int root = toInt(t3.read(ROOT));
                 System.out.println("ROOT = " + root);
-                int child = Bytes.toInt(t3.read(CHILD + index).array());
+                int child = toInt(t3.read(CHILD + index));
                 System.out.println(CHILD + index + " = " + child);
 
-                t3.write(ROOT, ByteBuffer.wrap(Bytes.toBytes(root + child)));
-                t3.write(CHILD + index, ByteBuffer.wrap(Bytes.toBytes(0)));
+                t3.write(ROOT, toBytes(root + child));
+                t3.write(CHILD + index, toBytes(0));
                 t3.commit();
                 System.out.println("Transaction 3 completed by Thread-" + index);
             } catch (TransactionException e) {
